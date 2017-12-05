94ab6df463a759713b4eeea481f47ac7dc649830
Peter Ringelmann <peterringelmann@gmail.com>
2017-12-05 11:15:54 +0200

    removed json encoding

--- src/utils/network.js
+++ src/utils/network.js
@@ -1,93 +1,85 @@
 // @ts-check
-import {
-    MESSAGE_ADD,
-    MESSAGE_SEND,
-    PASSTHROUGH_SEND,
-    PASSTHROUGH_RECEIVE
-} from '../actionTypes';
+import { MESSAGE_ADD, MESSAGE_SEND, PASSTHROUGH_SEND, PASSTHROUGH_RECEIVE } from '../actionTypes';
 
 import * as connectionActions from '../actions/connection';
 import * as messageActions from '../actions/messages';
 
 /** A general network manager utility class that handles message sending and receiving through a supplied transport */
 class NetworkManager {
-    /**
+	/**
      * Create a point.
      * @param {object} arguments
      * @param {object} arguments.store - The redux store to dispatch actions against
      * @param {object} arguments.client - The transport client to use for network communication
      */
-    constructor({ store, client }) {
-        this.dispatch = store.dispatch;
-        this.client = client;
-        this.store = store;
+	constructor({ store, client }) {
+		this.dispatch = store.dispatch;
+		this.client = client;
+		this.store = store;
 
-        this.messageSendHandler = this.messageSendHandler.bind(this);
-        this.passThroughSendHandler = this.passThroughSendHandler.bind(this);
-    }
+		this.messageSendHandler = this.messageSendHandler.bind(this);
+		this.passThroughSendHandler = this.passThroughSendHandler.bind(this);
+	}
 
-    /**
+	/**
      * Initilise the network transport and bind event handlers
      */
-    init() {
-        this.client
-            .init({
-                message: this.messageReceiveHandler.bind(this),
-                passThrough: this.passThroughReceiveHandler.bind(this),
-                connection: {
-                    close: this.connectionCloseHandler.bind(this),
-                    attempt: this.connectionAttemptHandler.bind(this),
-                    open: this.connectionOpenHandler.bind(this)
-                }
-            })
-            .then(() => this.bindActionEvents());
-    }
+	init() {
+		this.client
+			.init({
+				message: this.messageReceiveHandler.bind(this),
+				passThrough: this.passThroughReceiveHandler.bind(this),
+				connection: {
+					close: this.connectionCloseHandler.bind(this),
+					attempt: this.connectionAttemptHandler.bind(this),
+					open: this.connectionOpenHandler.bind(this)
+				}
+			})
+			.then(() => this.bindActionEvents());
+	}
 
-    messageReceiveHandler(message) {
-        this.dispatch(messageActions.messageReceive(message));
-    }
+	messageReceiveHandler(message) {
+		this.dispatch(messageActions.messageReceive(message));
+	}
 
-    passThroughReceiveHandler(message) {
-        this.dispatch({
-            type: PASSTHROUGH_RECEIVE,
-            payload: message
-        });
-    }
+	passThroughReceiveHandler(message) {
+		this.dispatch({
+			type: PASSTHROUGH_RECEIVE,
+			payload: message
+		});
+	}
 
-    connectionOpenHandler() {
-        this.dispatch(connectionActions.established());
-    }
+	connectionOpenHandler() {
+		this.dispatch(connectionActions.established());
+	}
 
-    connectionCloseHandler() {
-        this.dispatch(connectionActions.dropped());
-    }
+	connectionCloseHandler() {
+		this.dispatch(connectionActions.dropped());
+	}
 
-    connectionAttemptHandler() {
-        this.dispatch(connectionActions.attempted());
-    }
+	connectionAttemptHandler() {
+		this.dispatch(connectionActions.attempted());
+	}
 
-    messageSendHandler({ detail: { payload } }) {
-        this.client.send(JSON.stringify(payload));
-    }
+	messageSendHandler({ detail: { payload } }) {
+		this.client.send(payload);
+	}
 
-    passThroughSendHandler({ detail: { payload } }) {
-        this.client.send(JSON.stringify(payload));
-    }
+	passThroughSendHandler({ detail: { payload } }) {
+		this.client.send(payload);
+	}
 
-    /**
+	/**
      * Add global event handlers for messages/passthroughs triggered outside of RWC.
      */
-    bindActionEvents() {
-        let { eventNamespace } = this.store.getState().config.network;
-        window.addEventListener(
-            `${eventNamespace}-${MESSAGE_SEND}`,
-            this.messageSendHandler
-        );
-        window.addEventListener(
-            `${eventNamespace}-${PASSTHROUGH_SEND}`,
-            this.passThroughSendHandler
-        );
-    }
+	bindActionEvents() {
+		let { eventNamespace } = this.store.getState().config.network;
+		window.addEventListener(`${eventNamespace}-${MESSAGE_SEND}`, this.messageSendHandler);
+		window.addEventListener(
+			`${eventNamespace}-${PASSTHROUGH_SEND}`,
+			this.passThroughSendHandler
+		);
+	}
 }
 
 export default NetworkManager;
