7118567c1256d3ccf285fbca929b158e975518d6
Alecia Bester <>
2025-04-07 13:56:45 +0200

    fix: exports on JS

--- src/themes/unstyled/index.ts
+++ src/themes/unstyled/index.ts
@@ -1,7 +1,11 @@
 import { Message } from '../../components/Message';
 import { InputArea } from '../../components/InputArea';
+import type { FC } from 'react';
 
 export default {
   MessageComponent: Message,
   InputComponent: InputArea,
-}; 
+} as {
+  MessageComponent: FC<any>;
+  InputComponent: FC<any>;
+};
